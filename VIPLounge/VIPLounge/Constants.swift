3e2c679971bbc5b4211804b3c4f8fa6c23aaa78a
ElSteverino <sdmarcotte@gmail.com>
2018-04-24 17:44:24 -0500

    Initial commit

---
+++ VIPLounge/VIPLounge/Constants.swift
@@ -0,0 +1,40 @@
+// MARK: Change Log
+/*************************************************************************************************************************************
+ 
+ Constants.swift
+ 
+ DESCRIPTION
+ 
+ 
+ 2018-04-02 <sdm>
+ • Initial Code
+ 
+ *************************************************************************************************************************************/
+
+import Foundation
+
+
+enum Constant {
+    
+    // VIP Code boilerPlate resource
+    static let boilerPlateName = "CodeBoilerPlate"
+    static let boilerPlateType = "rtf"
+
+}
+
+
+
+
+
+// Public Domain, 2018 by Steve Marcotte
+//
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, distribute, sublicense, create a derivative work,
+// and/or sell copies of the Software in any work that is designed, intended, or marketed for pedagogical or instructional purposes
+// related to programming, coding, application development, or information technology.  Permission for such use, copying, modification,
+// merger, publication, distribution, sublicensing, creation of derivative works, or sale is expressly withheld.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ VIPLounge/VIPLoungeTests/VIPLoungeTests.swift
@@ -0,0 +1,59 @@
+// MARK: Change Log
+/*************************************************************************************************************************************
+ 
+ VIPLoungeTests.swift
+ 
+ DESCRIPTION
+ 
+ 
+ 2018-04-02 <sdm>
+ • Initial Code
+ 
+ *************************************************************************************************************************************/
+
+import XCTest
+@testable import VIPLounge
+
+
+class VIPLoungeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
+
+
+
+
+
+// Public Domain, 2018 by Steve Marcotte
+//
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, distribute, sublicense, create a derivative work,
+// and/or sell copies of the Software in any work that is designed, intended, or marketed for pedagogical or instructional purposes
+// related to programming, coding, application development, or information technology.  Permission for such use, copying, modification,
+// merger, publication, distribution, sublicensing, creation of derivative works, or sale is expressly withheld.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
